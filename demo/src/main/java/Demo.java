fc5958a5b80ad0607743bc36fa75797e6c657c77
lijie <925028680@qq.com>
2020-05-11 21:07:20 +0800

    dw2

--- demo/src/main/java/Demo.java
+++ demo/src/main/java/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println("say hello how are you?");
-        System.out.println("I am fine");//我是程序员A/.111
+        System.out.println("I am fine");
         System.out.println("are you good");//第二次修改
 
         System.out.println("--------");//123456
